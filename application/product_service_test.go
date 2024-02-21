8ba5bbcee51aaa4a9ea19630e40b28f67fa69518
Luan Fernandes <souluanf@icloud.com>
2024-02-21 20:14:25 -0300

    finishing implementation of the service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -25,3 +25,43 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mockapplication.NewMockProductInterface(ctrl)
+	persistence := mockapplication.NewMockProductPersistenceInterface(ctrl)
+
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("abc", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_EnableDisable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mockapplication.NewMockProductInterface(ctrl)
+
+	product.EXPECT().Enable().Return(nil).AnyTimes()
+	product.EXPECT().Disable().Return(nil).AnyTimes()
+
+	persistence := mockapplication.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
