b6fb274f2e16a303f7b086d7c5aa95849d4b3138
Luan Fernandes <souluanf@icloud.com>
2024-02-22 20:26:38 -0300

    creating Create handler

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -4,14 +4,20 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/souluanf/hexagonal-arch-golang/application"
+	"github.com/souluanf/hexagonal-arch-golang/dto"
 	"github.com/urfave/negroni"
 	"net/http"
 )
 
 func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+
 	r.Handle("/products/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/products", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -30,6 +36,36 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+		var productDto dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		product, err := service.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(product)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
 			return
 		}
 
--- dto/product.go
+++ dto/product.go
@@ -9,6 +9,10 @@ type Product struct {
 	Status string  `json:"status"`
 }
 
+func NewProduct() *Product {
+	return &Product{}
+}
+
 func (p *Product) Bind(product *application.Product) (*application.Product, error) {
 	if p.Id != "" {
 		product.Id = p.Id
