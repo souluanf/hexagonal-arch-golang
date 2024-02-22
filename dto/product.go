04bc00a6d78cf53ccfcd31b1cee05932a0c5abd2
Luan Fernandes <souluanf@icloud.com>
2024-02-22 20:21:01 -0300

    working with DTO

---
+++ dto/product.go
@@ -0,0 +1,27 @@
+package dto
+
+import "github.com/souluanf/hexagonal-arch-golang/application"
+
+type Product struct {
+	Id     string  `json:"id"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+	Status string  `json:"status"`
+}
+
+func (p *Product) Bind(product *application.Product) (*application.Product, error) {
+	if p.Id != "" {
+		product.Id = p.Id
+	}
+
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = p.Status
+
+	_, err := product.IsValid()
+	if err != nil {
+		return &application.Product{}, err
+	}
+
+	return product, nil
+}
