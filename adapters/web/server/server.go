7512dda2d6db1027f158e592d1cb7fe17e7c051c
Luan Fernandes <souluanf@icloud.com>
2024-02-22 19:44:38 -0300

    creating web server

---
+++ adapters/web/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/souluanf/hexagonal-arch-golang/application"
+	"github.com/urfave/negroni"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger())
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.22
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.8.4
+	github.com/urfave/negroni v1.0.0
 	go.uber.org/mock v0.4.0
 )
 
@@ -14,7 +17,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
